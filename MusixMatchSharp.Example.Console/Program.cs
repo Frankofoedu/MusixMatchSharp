6a7edfd6d71d9d9e7e3a17290bd6c2d4980d96e9
Ofoedu Ebuka <frankofoedu@gmail.com>
2019-03-29 18:19:24 +0100

    created example

---
+++ MusixMatchSharp.Example.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MusixMatchSharp.Example.ConsoleApp
+{
+    class Program
+    {
+        static string apikey = "XXXXXXXXXXXXXX";
+        static void Main(string[] args)
+        {
+            Musixmatch mx = new Musixmatch(apikey);
+
+            //Get the lyrics for Somebody That I Used to Know (Gotye feat. Kimbra)
+           var lyr = mx.Track_lyrics_get("15953433");
+
+            Console.WriteLine(lyr);
+        }
+    }
+}
